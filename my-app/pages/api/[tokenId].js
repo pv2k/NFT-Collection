9e8fab00eda50c08d78d626ec9eed43367fcebcb
Vidhey PV <vidhey96@gmail.com>
2022-09-29 15:45:41 +0530

    Update [tokenId].js

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+      "https://www.pngitem.com/pimgs/m/515-5153177_one-piece-franky-jolly-roger-hd-png-download.png";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
